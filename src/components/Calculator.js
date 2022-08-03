3e57241a11589fe921554537cc16522f396cf317
Htet Naing <htetnaing0814@gmail.com>
2022-08-03 23:19:05 +0630

    refactored calculator class into function with hooks

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,7 +1,7 @@
 /* eslint-disable no-eval */
 /* eslint-disable max-len */
 
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import '../index.css';
 
@@ -17,85 +17,63 @@ const buttonStyle = (bgColor, w) => ({
   borderTop: 'none',
   borderRight: 'none',
 });
-// I mean functions
-class Calculator extends React.Component {
-  constructor(props) {
-    super(props);
-    this.setValue = this.setValue.bind(this);
-    this.state = {
-      value: '0',
-    };
-  }
 
-  setValue(newValue) {
+const Calculator = () => {
+  const [value, setValue] = useState('0');
+
+  const updateValue = (newValue) => {
     const ops = ['+', '-', '*', '/', '%'];
     if (newValue === '=') {
-      this.setState((oldState) => ({
-        value: ops.includes(oldState.value[oldState.value.length - 1]) ? oldState.value : `${eval(oldState.value)}`,
-      }));
+      setValue(ops.includes(value[value.length - 1]) ? value : `${eval(value)}`);
     } else if (newValue === 'AC') {
-      this.setState({
-        value: '0',
-      });
+      setValue('0');
     } else if (newValue === 'DEL') {
-      this.setState((oldState) => ({
-        value: oldState.value.length === 1 ? '0' : oldState.value.slice(0, -1),
-      }));
+      setValue(value.length === 1 ? '0' : value.slice(0, -1));
     } else if (ops.includes(newValue)) {
-      this.setState((oldState) => ({
-        value: oldState.value === '0' || ops.includes(oldState.value[oldState.value.length - 1]) ? oldState.value : oldState.value + newValue,
-      }));
+      setValue(value === '0' || ops.includes(value[value.length - 1]) ? value : value + newValue);
     } else if (newValue === '.') {
-      let { value } = this.state;
       let index = '';
       for (let i = 0; i < value.length; i += 1) {
         if (ops.includes(value[i])) {
           index = value.lastIndexOf(value[i]) + 1;
         }
       }
-      value = value.substring(index);
-      this.setState((oldState) => ({
-        value: value.includes('.') ? oldState.value : oldState.value + newValue,
-      }));
+      const temp = value.substring(index);
+      setValue(temp.includes('.') ? value : value + newValue);
     } else {
-      this.setState((oldState) => ({
-        value: oldState.value === '0' ? newValue : oldState.value + newValue,
-      }));
+      setValue(value === '0' ? newValue : value + newValue);
     }
-  }
+  };
 
-  render() {
-    const { value } = this.state;
-    return (
-      <div>
-        <Display value={value} />
-        <Button label="AC" setValue={this.setValue} />
-        <Button label="DEL" setValue={this.setValue} />
-        <Button label="%" setValue={this.setValue} />
-        <Button label="/" bgColor="orange" setValue={this.setValue} />
-        <br />
-        <Button label="7" setValue={this.setValue} />
-        <Button label="8" setValue={this.setValue} />
-        <Button label="9" setValue={this.setValue} />
-        <Button label="*" bgColor="orange" setValue={this.setValue} />
-        <br />
-        <Button label="4" setValue={this.setValue} />
-        <Button label="5" setValue={this.setValue} />
-        <Button label="6" setValue={this.setValue} />
-        <Button label="-" bgColor="orange" setValue={this.setValue} />
-        <br />
-        <Button label="1" setValue={this.setValue} />
-        <Button label="2" setValue={this.setValue} />
-        <Button label="3" setValue={this.setValue} />
-        <Button label="+" bgColor="orange" setValue={this.setValue} />
-        <br />
-        <Button label="0" setValue={this.setValue} width="200px" />
-        <Button label="." setValue={this.setValue} />
-        <Button label="=" bgColor="orange" setValue={this.setValue} />
-      </div>
-    );
-  }
-}
+  return (
+    <div>
+      <Display value={value} />
+      <Button label="AC" updateValue={() => updateValue('AC')} />
+      <Button label="DEL" updateValue={() => updateValue('DEL')} />
+      <Button label="%" updateValue={() => updateValue('%')} />
+      <Button label="/" bgColor="orange" updateValue={() => updateValue('/')} />
+      <br />
+      <Button label="7" updateValue={() => updateValue('7')} />
+      <Button label="8" updateValue={() => updateValue('8')} />
+      <Button label="9" updateValue={() => updateValue('9')} />
+      <Button label="*" bgColor="orange" updateValue={() => updateValue('*')} />
+      <br />
+      <Button label="4" updateValue={() => updateValue('4')} />
+      <Button label="5" updateValue={() => updateValue('5')} />
+      <Button label="6" updateValue={() => updateValue('6')} />
+      <Button label="-" bgColor="orange" updateValue={() => updateValue('-')} />
+      <br />
+      <Button label="1" updateValue={() => updateValue('1')} />
+      <Button label="2" updateValue={() => updateValue('2')} />
+      <Button label="3" updateValue={() => updateValue('3')} />
+      <Button label="+" bgColor="orange" updateValue={() => updateValue('+')} />
+      <br />
+      <Button label="0" updateValue={() => updateValue('0')} width="200px" />
+      <Button label="." updateValue={() => updateValue('.')} />
+      <Button label="=" bgColor="orange" updateValue={() => updateValue('=')} />
+    </div>
+  );
+};
 
 const Display = (props) => {
   const { value } = props;
@@ -111,14 +89,14 @@ const Button = (props) => {
     label,
     bgColor,
     width,
-    setValue,
+    updateValue,
   } = props;
 
-  return <button style={buttonStyle(bgColor, width)} type="button" onClick={() => setValue(label)} value={label}>{ label }</button>;
+  return <button style={buttonStyle(bgColor, width)} type="button" onClick={() => updateValue()}>{ label }</button>;
 };
 
 Button.propTypes = {
-  setValue: PropTypes.func.isRequired,
+  updateValue: PropTypes.func.isRequired,
   label: PropTypes.string.isRequired,
   bgColor: PropTypes.string,
   width: PropTypes.string,
