78178bf7cd05229046caf5e016c59ab76f5ac2af
Htet Naing <htetnaing0814@gmail.com>
2022-08-09 16:29:39 +0630

    added operation and route tests

--- src/components/Display.js
+++ src/components/Display.js
@@ -2,15 +2,11 @@ import PropTypes from 'prop-types';
 
 const Display = (props) => {
   const { value } = props;
-  return <input readOnly  id="display" type="text" value={value} />;
+  return <input readOnly id="display" type="text" value={value} />;
 };
 
 Display.propTypes = {
   value: PropTypes.string.isRequired,
 };
-
-Display.defaultProps = {
-  value: 0,
-}
 
 export default Display;
--- src/tests/Calculator.test.js
+++ src/tests/Calculator.test.js
@@ -1,24 +1,57 @@
-import Calculator from '../components/Calculator';
-import Display from '../components/Display';
-
-import React from 'react';
+import '@testing-library/jest-dom';
+import userEvent from '@testing-library/user-event';
 import { render, screen, fireEvent } from '@testing-library/react';
+import Calculator from '../components/Calculator';
+import App from '../App';
 
 const labels = ['AC', 'DEL', '%', '/', '*', '-', '+', '=', '.', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+const numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+const ops = ['AC', 'DEL', '%', '/', '*', '-', '+', '=', '.'];
 
-describe('Display', () => {
+describe('Calculator', () => {
   test('renders Display component', () => {
     render(<Calculator />);
-    fireEvent.change(screen.getByRole('textbox'), {
-      target: { value: 'Display' },
-    });
+    expect(screen.getByRole('textbox')).toBeInTheDocument();
   });
-  labels.forEach(label => {
+
+  labels.forEach((label) => {
     test(`renders ${label} Button component`, () => {
       render(<Calculator />);
-      fireEvent.change(screen.getByText(label), {
-        target: { value: label },
-      });
+      expect(screen.getByText(label)).toBeInTheDocument();
     });
   });
 });
+
+describe('Operations', () => {
+  numbers.forEach((number) => {
+    test(`clicks ${number} Button component`, () => {
+      render(<Calculator />);
+      fireEvent.click(screen.getByText(number));
+      expect(screen.getByRole('textbox').value).toBe(number);
+    });
+  });
+
+  ops.forEach((op) => {
+    test(`clicks ${op} Button component`, () => {
+      render(<Calculator />);
+      const initialValue = screen.getByRole('textbox').value;
+      fireEvent.click(screen.getByText(op));
+      const { value } = screen.getByRole('textbox');
+      if (initialValue !== '0') {
+        expect(value).toBe(op);
+      }
+    });
+  });
+});
+
+test('full app rendering/navigating', async () => {
+    render(<App />)
+
+    expect(screen.getByText(/Welcome to the World of Mathgic! ✨/i)).toBeInTheDocument()
+
+    await userEvent.click(screen.getByText(/Calculator/i))
+    expect(screen.getByText(/Let's perform some MATHGIC !!!/i)).toBeInTheDocument()
+
+    await userEvent.click(screen.getByText(/Quote/i))
+    expect(screen.getByText(/Mathematics is not about numbers, equations, computations or algorithms: it is about understanding. - William Paul Thurston/i)).toBeInTheDocument()
+  })
