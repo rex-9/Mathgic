a4a39016c597798b20da3e50b08c10ca78d2e82f
Htet Naing <htetnaing0814@gmail.com>
2022-08-19 23:31:47 +0630

    add about contacts

--- src/components/Quote.js
+++ src/components/Quote.js
@@ -27,6 +27,30 @@ const Quote = () => {
       <button type="button" className="generateQuote bn25" onClick={updateQuote}>
         Generate quote
       </button>
+      <div id="about">
+        <div>
+          <a href="https://github.com/HtetNaing0814">
+            <i
+              className="fa-brands fa-github fa-2x"
+            />
+          </a>
+        </div>
+        <div className="whatsmail">
+          <i className="fa-brands fa-whatsapp fa-2x" />
+          <p>: +959443112251</p>
+        </div>
+        <div>
+          <a href="https://www.linkedin.com/in/htetnaing0814/">
+            <i
+              className="fa-brands fa-linkedin fa-2x"
+            />
+          </a>
+        </div>
+        <div className="whatsmail">
+          <i className="fa-solid fa-envelope fa-2x" />
+          <p>: htetnaing0814@gmail.com</p>
+        </div>
+      </div>
     </div>
   );
 };
