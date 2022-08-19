b7d736d2e26a22974adcc5f045eb8b36f6d24e04
Htet Naing <htetnaing0814@gmail.com>
2022-08-19 23:03:02 +0630

    add quote api

--- src/components/Quote.js
+++ src/components/Quote.js
@@ -1,11 +1,34 @@
-const Quote = () => (
-  <div id="quote">
-    Mathematics is not about numbers, equations, computations
-    <br />
-    <br />
-    {' '}
-    or algorithms: it is about understanding. - William Paul Thurston
-  </div>
-);
+import { useState } from 'react';
+
+const Quote = () => {
+  const [quotes, setQuote] = useState('');
+
+  const updateQuote = async () => {
+    const res = await fetch('https://random-math-quote-api.herokuapp.com/');
+
+    res
+      .json()
+      .then((res) => {
+        setQuote({
+          quotes: res.quote,
+        });
+      })
+      .catch();
+  };
+
+  const quote = [JSON.stringify(quotes)].join('').split('');
+  quote.pop();
+  quote.shift();
+  quote.splice(0, 9);
+
+  return (
+    <div id="quote">
+      <p>{quote}</p>
+      <button type="button" className="generateQuote bn25" onClick={updateQuote}>
+        Generate quote
+      </button>
+    </div>
+  );
+};
 
 export default Quote;
